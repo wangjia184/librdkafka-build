23fe9badcca731090f8b01cf55a408a552dc0243
Andreas Heider <andreas@heider.io>
2016-04-06 22:34:40 +0100

    Update static link patch for upstream changes

--- build.cmd
+++ build.cmd
@@ -13,7 +13,9 @@ echo Downloading nuget.exe
 echo Patching static linking
 cd librdkafka
 git reset --hard HEAD
+if %errorlevel% neq 0 exit /b %errorlevel%
 git apply ../0001-Statically-link-OpenSSL-and-MSVCR-on-Windows.patch
+if %errorlevel% neq 0 exit /b %errorlevel%
 cd ..
 
 echo Nuget restore
